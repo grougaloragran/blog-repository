e83070ab54c3a48cf60225df05976d650397f3db
Nebo Devcamp <nebograngerlomond@devcamp.com>
2017-07-12 14:41:00 +0000

    Completed authentication with virtual attrs for user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,3 +4,12 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 end
+  validates_presence_of :name
+  
+  def first_name
+    self.name.split.first
+  end
+  
+  def last_name
+    self.name.split.last
+  end
