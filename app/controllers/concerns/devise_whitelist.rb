a1c2b839f1d8a0f9b127b249a435c4767e35737a
Nebo Devcamp <nebograngerlomond@devcamp.com>
2017-07-17 14:56:54 +0000

    Added nav bar and added default header content

--- app/controllers/concerns/devise_whitelist.rb
+++ app/controllers/concerns/devise_whitelist.rb
@@ -1,5 +1,5 @@
 module DeviseWhitelist
-   extend ActiveSupport::Concern
+     extend ActiveSupport::Concern
    
    included do
     before_action :configure_permitted_parameters, if: :devise_controller?
@@ -8,5 +8,5 @@ module DeviseWhitelist
      def configure_permitted_parameters
         devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
         devise_parameter_sanitizer.permit(:account_update, keys: [:name])
-     end
+     end 
 end
