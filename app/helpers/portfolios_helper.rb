306ce692561954f7cdc2cd9053b20f905c5ea121
Nebo Devcamp <nebograngerlomond@devcamp.com>
2017-08-10 17:18:51 +0000

    updated file forms and refactored placeholder functionality into view

--- app/helpers/portfolios_helper.rb
+++ app/helpers/portfolios_helper.rb
@@ -1,2 +1,15 @@
 module PortfoliosHelper
+   def image_generater(height:, width:)
+    "http://placehold.it/#{height}x#{width}"
+   end
+   
+    def portfolio_img img, type
+        if img.model.main_image? || img.model.thumb_image?
+          img
+        elsif type == "thumb"
+            image_generater(height: '350', width: '200' )
+        elsif type == "main"
+            image_generater(height: '600', width: '400')
+        end
+    end
 end
